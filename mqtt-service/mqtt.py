65a9bf7a6a35b60070be5d11b99088d81a83fe9e
Giovanna Souza <gst.eng19@uea.edu.br>
2022-09-26 17:42:42 -0400

    teste2 - subscribe mqtt

--- mqtt-service/mqtt.py
+++ mqtt-service/mqtt.py
@@ -1,12 +1,13 @@
 from paho.mqtt.client import Client
+import paho.mqtt.client as paho
+from paho import mqtt
 import os
 class Mqtt:
     def __init__(self):
-        self.client = Client('ru-app')
-    
+        self.client = Client(client_id='', userdata=None, protocol= paho.MQTTv5)
+
     def on_connect(self, client, flags, rc, properties):
         print('Connected')
-        client.subscribe('topico_teste')
         
     def on_message(self, cls, topic, payload, client, qos):
         print(f'recebendo {payload}')
@@ -23,14 +24,27 @@ class Mqtt:
 
     async def main(self):
         self.client.on_connect = self.on_connect
+
+        print('caiuuuu')
+        
+        # enable TLS for secure connection
+        self.client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+        # set username and password
+        self.client.username_pw_set('ruapp', 'ruapp123')
+        # connect to HiveMQ Cloud on port 8883 (default for MQTT)
+        self.client.connect('1585424bfe794383997ea302ee1d1e4e.s1.eu.hivemq.cloud', 8883)
+
         self.client.on_message = self.on_message
         self.client.on_disconnect = self.on_disconnect
         self.client.on_subscribe = self.on_subscribe
+
+        self.client.subscribe('my/teste')
+        self.client.loop_forever()
         # user = os.getenv('MQTT_USER')
         # password = os.getenv('MQTT_PASSWORD')
-        host = os.getenv('MQTT_BROKER_HOST')
-        print(host)
-        self.client.connect(host)
+        #host = os.getenv('MQTT_BROKER_HOST')
+        #print(host)
+        #self.client.connect(host)
         
 
 
--- mqtt-service/teste.py
+++ mqtt-service/teste.py
@@ -4,13 +4,7 @@ import asyncio
 m = Mqtt()
 loop = asyncio.get_event_loop()
 
-def teste_publish():
-    print('enviando')
-    m.client.publish("topico_teste","enviando")
-
 if __name__ == "__main__":
     loop.run_until_complete(m.main())
-    teste_publish()
     loop.run_forever()
-    
     
