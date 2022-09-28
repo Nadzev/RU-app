12af5883693619e9e0ec842092869335622906d0
Giovanna Souza <gst.eng19@uea.edu.br>
2022-09-28 13:34:02 -0400

    Bug fix: mensagem não aparecia no LCD

--- src/mainrasp.py
+++ src/mainrasp.py
@@ -21,7 +21,11 @@ lcd_d7 = digitalio.DigitalInOut(board.D22)
 
 lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,
                                       lcd_d7, lcd_columns, lcd_rows)
-lcd.message("OOOOOOOOOOOOI")
+while True:
+
+    lcd.message = "OOOOOOOOI"
+    sleep(2)
+
 
 # def setup():
 
